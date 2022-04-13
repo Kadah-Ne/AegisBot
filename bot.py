2bc13bfd0ca687bed79ceca705693da7702743e2
Kadah-Ne <martin.gouviaux@epsi.fr>
2022-04-13 09:13:36 +0200

    Added music features, see todo-list.

--- bot.py
+++ bot.py
@@ -2,16 +2,17 @@
 
 from inspect import getcallargs
 import discord
-from discord import guild
-from discord import channel
-from discord.ext import commands
+from discord import guild,channel
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 import os
 from discord.utils import get
 import time
 from datetime import datetime, datetime, date
-from discord.ext import tasks
 
+######### BETA
+import youtube_dl
+#########
 
 #Initialisation des variables Globales
 
@@ -181,7 +182,7 @@ def updateEventFile(id : str):
 
 ########### Fonctions de gestion du server
 
-#Fonction qui supprime tout les messages du channel *
+#Fonction qui supprime 10 messages du channel
 @bot.command(name = "Purge")
 @commands.has_permissions(manage_messages=True)
 async def deletAllMessages(ctx, channelID : discord.channel = None):
@@ -250,7 +251,106 @@ async def pref(ctx,NEWPREFIX : str = None):
     else:
         await ctx.channel.send(f"Mon prefix actuel est : `{PREFIX}`")
 
+########### Fonctions pour les utilisateurs (BETA)
 
+#Definition des options de ytdl (A BOUGER EN HAUT UNE FOIS SORTI DE BETA)
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        return filename
+
+# Commande permettant au bot de rejoindre un channel vocal
+@bot.command(name='join', help='Tells the bot to join the voice channel')
+async def join(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+# Commande permettant au bot de quitter un channel vocal
+@bot.command(name='leave', help='To make the bot leave the voice channel')
+async def leave(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+# Commande envoyant la requete de la musique (Faire en sorte que on ne soit pas obligé de mettre un url)
+@bot.command(name='play_song', help='To play song')
+async def play(ctx,url):
+    try :
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+
+        async with ctx.typing():
+            filename = await YTDLSource.from_url(url, loop=bot.loop)
+            voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
+        await ctx.send('**Now playing:** {}'.format(filename))
+    except:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+# Commande mettant en pause la musique
+@bot.command(name='pause', help='This command pauses the song')
+async def pause(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.pause()
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
+
+# Commande relancant la musique    
+@bot.command(name='resume', help='Resumes the song')
+async def resume(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_paused():
+        await voice_client.resume()
+    else:
+        await ctx.send("The bot was not playing anything before this. Use play_song command")
+
+# Commande arretant la musique
+@bot.command(name='stop', help='Stops the song')
+async def stop(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.stop()
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
 
 
 

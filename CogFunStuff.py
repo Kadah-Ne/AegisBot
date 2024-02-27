aa4774a2e550a30dacbad497fc9f152d1c8d4d37
Kadah-Ne <mgouviaux@gmail.com>
2024-02-27 01:34:13 +0100

    aaa

--- CogFunStuff.py
+++ CogFunStuff.py
@@ -11,18 +11,48 @@ class CogFunStuff(commands.Cog):
     
     @commands.command (name="roll", aliases = ["Roll","rolls","Rolls"])
     async def roll(self,ctx,die : str):
+        mod = -1
+        numlist = []
+        sides_mod = re.split("d",die,flags=re.IGNORECASE)[-1]
+        if sides_mod.__contains__('+'):
+            sides = re.split("+",sides_mod,flags=re.IGNORECASE)[0]
+            mod = re.split("+",sides_mod,flags=re.IGNORECASE)[-1]
+            item = "+"
+        elif sides_mod.__contains__('-'):
+            sides = re.split("-",sides_mod,flags=re.IGNORECASE)[0]
+            mod = re.split("-",sides_mod,flags=re.IGNORECASE)[-1]
+            item = "-"
         try :
-            sides = int(re.split("d",die,flags=re.IGNORECASE)[-1])
+            sides = int(sides)
+            if mod != -1:
+                mod = int(mod)
             occ = int(re.split("d",die,flags=re.IGNORECASE)[0])
             number = 0
             if not occ:
                 occ = 1
             for i in range (occ):
-                number += random.randint(1,sides)
+                rand = random.randint(1,sides)
+                numlist.append(rand)
+                number += rand
+            match item:
+                case "+" :
+                    number += mod
+                case "-" :
+                    number -= mod
+
             if occ == 1 :
-                await ctx.channel.send(f"you rolled a {number} on the D{sides}")
+                numlist.sort()
+                textchain = ""
+                for i in numlist: 
+                        textchain += f"+ {i} "
+                    
+                
+                textchain = textchain[0:]
+                await ctx.channel.send(f"you rolled a {number}({textchain} {item} {mod}) on the D{sides}")
             else : 
-                await ctx.channel.send(f"you rolled a {number} on the {occ} D{sides}")
+                await ctx.channel.send(f"you rolled a {number}({textchain} {item} {mod}) on the {occ} D{sides}")
+            
+            
         except :
-            await ctx.channel.send("Utilisez le format [x]D[y] pour la commande ou x et y sont des nombres entiers")
+            await ctx.channel.send("Utilisez le format [x]D[y]+/-[z] pour la commande ou x,y et z sont des nombres entiers")
         

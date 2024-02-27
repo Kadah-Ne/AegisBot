35518b3d107c38bb16fdcbff78f57c29ddad3eb1
Kadah-Ne <mgouviaux@gmail.com>
2024-02-27 01:52:53 +0100

    aaa

--- CogFunStuff.py
+++ CogFunStuff.py
@@ -13,6 +13,8 @@ class CogFunStuff(commands.Cog):
     async def roll(self,ctx,die : str):
         mod = -1
         numlist = []
+        occ = -1
+        item =""
         sides_mod = re.split("d",die,flags=re.IGNORECASE)[-1]
         if sides_mod.__contains__('+'):
             sides = re.split("+",sides_mod,flags=re.IGNORECASE)[0]
@@ -22,23 +24,32 @@ class CogFunStuff(commands.Cog):
             sides = re.split("-",sides_mod,flags=re.IGNORECASE)[0]
             mod = re.split("-",sides_mod,flags=re.IGNORECASE)[-1]
             item = "-"
+        else : 
+            sides = re.split("-",sides_mod,flags=re.IGNORECASE)[0]
+        
         try :
             sides = int(sides)
+            
             if mod != -1:
                 mod = int(mod)
-            occ = int(re.split("d",die,flags=re.IGNORECASE)[0])
+            if (re.split("d",die,flags=re.IGNORECASE)[0]) :
+                 
+                occ = int(re.split("d",die,flags=re.IGNORECASE)[0])
+            
             number = 0
-            if not occ:
+            if occ == -1:
                 occ = 1
+            
             for i in range (occ):
                 rand = random.randint(1,sides)
                 numlist.append(rand)
                 number += rand
+            print(item)
             if item == "+":
                 number += mod
             elif item == "-" :
                 number -= mod
-
+            print(occ)
             if occ == 1 :
                 numlist.sort()
                 textchain = ""
@@ -46,11 +57,14 @@ class CogFunStuff(commands.Cog):
                         textchain += f"+ {i} "
                 
                 textchain = textchain[0:]
-                await ctx.channel.send(f"you rolled a {number}({textchain} {item} {mod}) on the D{sides}")
+                if item == "":
+                    print(f"you rolled a {number} on the D{sides}")
+                else : 
+                     print(f"you rolled a {number}({numlist[0]} {item} {mod}) on the D{sides}")
             else : 
-                await ctx.channel.send(f"you rolled a {number}({textchain} {item} {mod}) on the {occ} D{sides}")
+                print(f"you rolled a {number}({textchain} {item} {mod}) on the {occ} D{sides}")
             
             
         except :
-            await ctx.channel.send("Utilisez le format [x]D[y]+/-[z] pour la commande ou x,y et z sont des nombres entiers")
+            print("Utilisez le format [x]D[y]+/-[z] pour la commande ou x,y et z sont des nombres entiers")
         

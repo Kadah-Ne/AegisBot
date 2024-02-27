f46d92fa549773bfbb2c23b01205cafc9fb70c01
Kadah-Ne <mgouviaux@gmail.com>
2024-02-27 01:18:50 +0100

    multy die

--- CogFunStuff.py
+++ CogFunStuff.py
@@ -11,7 +11,18 @@ class CogFunStuff(commands.Cog):
     
     @commands.command (name="roll", aliases = ["Roll","rolls","Rolls"])
     async def roll(self,ctx,die : str):
-        await ctx.channel.send("Test")
-        sides = int(re.split("d",die,flags=re.IGNORECASE)[-1])
-        number = random.randint(1,sides)
-        await ctx.channel.send(f"you rolled a {number} on the D{sides}")
+        try :
+            sides = int(re.split("d",die,flags=re.IGNORECASE)[-1])
+            occ = int(re.split("d",die,flags=re.IGNORECASE)[0])
+            number = 0
+            if not occ:
+                occ = 1
+            for i in range (occ):
+                number += random.randint(1,sides)
+            if occ == 1 :
+                await ctx.channel.send(f"you rolled a {number} on the D{sides}")
+            else : 
+                await ctx.channel.send(f"you rolled a {number} on the {occ} D{sides}")
+        except :
+            await ctx.channel.send("Utilisez le format [x]D[y] pour la commande ou x et y sont des nombres entiers")
+        

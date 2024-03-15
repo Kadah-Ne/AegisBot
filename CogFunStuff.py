7825f60ea401830afbd8b637a2ffcc94d27f9b9f
Kadah <mgouviaux@gmail.com>
2024-03-15 18:17:10 +0100

    d

--- CogFunStuff.py
+++ CogFunStuff.py
@@ -111,8 +111,9 @@ class CogFunStuff(commands.Cog):
                 await ctx.channel.send("Ici")
                 if len(re.findall(r'\+\d*',die)) > 0 :
                     mod = re.findall(r'\+\d*',die)[0]
+                await ctx.channel.send("Là")                    
                 finalTxt += f"{outputs[die][0]} : {outputs[die][1]}{mod} \n"
-                await ctx.channel.send("Là")
+                
             await ctx.channel.send(finalTxt)
             
         except :

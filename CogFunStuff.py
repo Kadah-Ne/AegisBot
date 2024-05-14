92d073656ca637c1f2dbbb46f241e6b6b459cfe6
Kadah-Ne <mgouviaux@gmail.com>
2024-05-14 21:14:23 +0200

    d

--- CogFunStuff.py
+++ CogFunStuff.py
@@ -125,7 +125,8 @@ class CogFunStuff(commands.Cog):
     @commands.command(name="inspiration", aliases = ["inspi","Inspiration","Inspi"])
     async def inspiration(self,ctx):
         listQuotes = ["Shoot for the moon! Even if you miss, you'll... something... something... stars!","You know who you remind me of? Me!","You've got a great personality!","You're doing pretty well!","You must be great -- you're hanging out with me!","You're not the ugliest person I've ever met!","NEVER limit yourself!","Hey... player...! You're really good at this game!","It's in our moments of decision that destiny is shaped!","You're -- uh -- special?","Follow your hearts... and stuff.","Don't belive in yourself, believe in me because I believe in you!"]
-        e = f"{random.choice(listQuotes)}".format("https://tenor.com/view/borderlands-inspired-skill-borderlands-inspired-gif-20917083")
+        e = discord.Embed(title=random.choice(listQuotes))
+        e.set_thumbnail(url="https://tenor.com/view/borderlands-inspired-skill-borderlands-inspired-gif-20917083")
         await ctx.channel.send(e)       
         
     

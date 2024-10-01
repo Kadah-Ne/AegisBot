d2386ab2d6070195297e2018bb45cf88b54ab11f
Kadah Ne <74606741+Kadah-Ne@users.noreply.github.com>
2024-10-01 15:51:31 +0200

    Update CogFunStuff.py
    Ajout d'une fonction extract pour recup les citationos d'une année

--- CogFunStuff.py
+++ CogFunStuff.py
@@ -136,7 +136,7 @@ class CogFunStuff(commands.Cog):
         else:
             await ctx.channel.send(f"https://tenor.com/view/borderlands-inspired-skill-borderlands-inspired-gif-20917083")
 
-    async def getCitations(self):
+    async def getCitations(self, year_selected: int = 0):
         channelId = 772904165189222410
         channel = self.bot.get_channel(channelId)
         msgs = [msg async for msg in channel.history(oldest_first=False)]
@@ -148,14 +148,17 @@ class CogFunStuff(commands.Cog):
             #     contenue = userCited + " " +i.content.split(">")[1]
             # else :
             #     contenue = i.content
-            contenue = i.content
-            if(i.attachments != []):
-                contenue+= f" {i.attachments[0]}"
-            listCita.append(contenue)
+            if i.created_at.date.year == year or year == 0:
+                contenue = i.content
+                if(i.attachments != []):
+                    contenue+= f" {i.attachments[0]}"
+                listCita.append(contenue)
         return listCita
 
-            
-
+    @commands.command (name="extract")
+        async def extract(self,ctx,year: int):
+            choiceCitations = await self.getCitations(year)
+            await ctx.channel.send(choiceCitations)
         
     
         

b69f06a048f5e1eb67ddaa2f4c0cc42f7e7e4901
Kadah-Ne <mgouviaux@gmail.com>
2024-11-07 10:14:57 +0100

    Updated spotify to toggle

--- CogSpotify.py
+++ CogSpotify.py
@@ -11,6 +11,7 @@ import os
 class CogSpotify(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
+        self.music_on = False
         load_dotenv()
         client_id = os.getenv("CLIENT_ID")
         client_secret = os.getenv("CLIENT_SECRET")
@@ -26,21 +27,32 @@ class CogSpotify(commands.Cog):
                 redirect_uri=redirect_uri,    
                 scope=scope, open_browser=False))
 
-    
+    @commands.command (name = "ToggleRM", aliases = ["toggle","trm"])
+    @commands.is_owner()
+    async def ToggleMusic(self,ctx) :
+        self.music_on = not self.music_on
+        if self.music_on :
+            await ctx.channel.send("Kadah prend des requetes")
+        else : 
+            await ctx.channel.send("Kadah ne prend plus de requetes")
+            
     @commands.command (name = "RequestMusic", aliases = ["RM","rm","Music","music"])
     async def RequestMusic(self,ctx,* message : str):
-        token_info = self.sp.auth_manager.get_access_token()
-        flag = spotipy.SpotifyOAuth.is_token_expired(token_info)
-        if(flag) :
-            self.sp.auth_manager.refresh_access_token(token_info['refresh_token'])
-            print("Refreshing Access Token.")
-        message = ' '.join(message)
-        results = self.sp.search(message, 1, 0, "track") 
-        songs_dict = results['tracks'] 
-        song_items = songs_dict['items'] 
-        song = song_items[0]['id'] 
-        # webbrowser.open(song) 
-        self.sp.start_playback(uris=[f'spotify:track:{song}'])
+        if self.music_on :
+            token_info = self.sp.auth_manager.get_access_token()
+            flag = spotipy.SpotifyOAuth.is_token_expired(token_info)
+            if(flag) :
+                self.sp.auth_manager.refresh_access_token(token_info['refresh_token'])
+                print("Refreshing Access Token.")
+            message = ' '.join(message)
+            results = self.sp.search(message, 1, 0, "track") 
+            songs_dict = results['tracks'] 
+            song_items = songs_dict['items'] 
+            song = song_items[0]['id'] 
+            # webbrowser.open(song) 
+            self.sp.start_playback(uris=[f'spotify:track:{song}'])
+        else :
+            await ctx.channel.send("Kadah ne prends pas de requetes de musique pour le moment")
 
 
         

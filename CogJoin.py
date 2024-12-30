edcef6b910b32242329c018a6eb144214a0fecc3
Kadah Ne <74606741+Kadah-Ne@users.noreply.github.com>
2024-12-30 16:51:08 +0100

    a

--- CogJoin.py
+++ CogJoin.py
@@ -6,16 +6,16 @@ class CogJoin(commands.Cog):
         self.Manager = Manager
 
     async def addRoleNew(self,target):
-        Role = get(target.guild.roles, name="Nouvel Arrivant")
+        Role = get(target.guild.roles, name="Random Members")
         await target.add_roles(Role)
     
-    # @commands.Cog.listener()
-    # async def on_member_join(self,member):
-    #     channel= member.guild.system_channel
-    #     if channel is not None:
-    #         await channel.send(f'Bienvenue {member.mention}.')
-    #     await self.addRoleNew(member)
-    #     await self.Manager.writeLogs(f"{member} as joined the server")
+    @commands.Cog.listener()
+    async def on_member_join(self,member):
+        channel= member.guild.system_channel
+        if channel is not None:
+            await channel.send(f'Bienvenue {member.mention}.')
+        await self.addRoleNew(member)
+        await self.Manager.writeLogs(f"{member} as joined the server")
     
     
         
